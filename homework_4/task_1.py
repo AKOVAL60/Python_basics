36bdd47ca299aacaeafd90543ee6027de3600f4d
GitEsense <koval@mdx39.ru>
2023-01-10 15:48:58 +0200

    homework_4 done...

--- homework_4/task_1.py
+++ homework_4/task_1.py
@@ -2,3 +2,17 @@
 # нём формулу: (выработка в часах*ставка в час) + премия. Во время выполнения расчёта для конкретных значений
 # необходимо запускать скрипт с параметрами.
 
+
+from sys import argv
+
+name, time, salary, bonus = argv
+
+try:
+    production = int(time)
+    salary = int(salary)
+    bonus = int(bonus)
+    total = time * salary + bonus
+    print(name)
+    print(f'заработная плата сотрудника  {total}')
+except ValueError:
+    print('Не введены числа')
